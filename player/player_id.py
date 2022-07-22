636c1cd017f7ff12a5269d466c6f2314b1a079bc
josh <joshua.didcote@outlook.com>
2022-07-22 16:27:48 +0100

    Refactored FindInitialPuuids into a more universal PlayerIds class

--- player/player_id.py
+++ player/player_id.py
@@ -1,52 +1,82 @@
-from typing import List
+from pathlib import Path
 
+import pandas as pd
 from riotwatcher import LolWatcher
 
 
-class FindInitialPuuids:
-    """
-    Finds unique puuids seeded from the initial puuid
-    """
-    def __init__(self, api_key: str, initial_puuid: List[str], region: str):
-        """
-        :param initial_puuid: Initial puuid to start player search
-        :param region: League region
-        """
-        self._initial_puuid = initial_puuid
-        self.puuids = initial_puuid
-        self.watcher = LolWatcher(api_key)
+class PlayerIds:
+    def __init__(self, region: str):
+        self.file_path = Path("Data/puuids.csv")
+        self.puuids = None
         self.region = region
 
-    def _match_puuids_single_game(self, match_id: str):
+    @property
+    def file_exists(self) -> bool:
+        return Path.is_file(self.file_path)
+
+    def load_puuids(self) -> None:
+        """
+        Loads the currently saved player ids and stores in puuids attribute
+        """
+        if not self.file_exists:
+            raise FileNotFoundError("No local file database found, please use set_db method to setup local database")
+        self.puuids = pd.read_csv(self.file_path)
+
+    def setup_db(self, puuid: str) -> None:
+        """
+        Sets up local database with a single puuid to get started
+        :param puuid:  single puuid to setup database
+        """
+        if self.file_exists:
+            print("Local database already exists, can't run setup")
+            return
+        puuids = pd.DataFrame({"puuid": puuid, "region": self.region}, index=[0])
+        puuids.to_csv(self.file_path, index=False)
+
+    def _match_puuids_single_game(self, watcher: LolWatcher, puuid: str):
+        """
+        Get the players in the last match played for a single puuid
+        """
+        last_match_id = watcher.match.matchlist_by_puuid(
+            region=self.region,
+            puuid=puuid,
+            count=1
+        )[0]
+
         match_players = (
-            self.watcher
+            watcher
                 .match
-                .timeline_by_match(self.region, match_id)
+                .timeline_by_match(self.region, last_match_id)
             ["info"]
             ["participants"]
         )
         return [x["puuid"] for x in match_players]
 
-    def _add_new_puuids(self, puuid: str, n_matches: int = 10):
-        match_history = self.watcher.match.matchlist_by_puuid(
-            region=self.region,
-            puuid=puuid,
-            count=n_matches
-        )
-        all_puuids = []
-        for match_id in match_history:
-            puuids = self._match_puuids_single_game(match_id)
-            all_puuids += puuids
-        self.puuids += all_puuids
-        self.puuids = list(set(self.puuids))
+    def find_new_puuids(self, api_key: str, n_puuids: int) -> None:
+        """
+        Finds new puuids seeded from the existing stored puuids
+        :param n_puuids: Number of additional puuids to add, wil likely go slightly over this number
+        """
 
-    def find_puuids(self, n_players=200):
-        """
-        Finds new player ids seeded from initial puuid.
-        Updates the puuids attribute
-        :param n_players: Number of players to search for - this can be slightly exceeded
-        """
-        i = 0
-        while len(self.puuids) <= n_players:
-            self._add_new_puuids(self.puuids[i])
-            i += 1
+        watcher = LolWatcher(api_key)
+
+        if self.puuids is None:
+            self.load_puuids()
+
+        n_players_found = 0
+
+        while n_players_found <= n_puuids:
+            puuid = self.puuids.iloc[-1]["puuid"]
+
+            # Get puuids of players last played with
+            new_puuids = self._match_puuids_single_game(watcher, puuid)
+
+            # Add to stored puuids and remove duplicates
+            new_rows = pd.DataFrame({"puuid": new_puuids})
+            new_rows["region"] = self.region
+            self.puuids = pd.concat([self.puuids, new_rows]).drop_duplicates().reset_index(drop=True)
+            n_players_found += new_rows["puuid"].nunique() - 1
+
+        print(f"{n_players_found} new players found. Writing to database...")
+
+        return
